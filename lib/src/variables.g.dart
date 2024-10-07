828eaf415a32b6cad991c7ffde3b79e7500fb965
unknown <muhammadnurramadhan920307@gmail.com>
2024-10-07 09:45:40 +0700

    up 7 oktober 2024

--- lib/src/variables.g.dart
+++ lib/src/variables.g.dart
@@ -4,9 +4,9 @@ class Variables {
   // static const ipv4_server = "https://indophotobox.com/selfie"; // cpanel url
   static const ipv4_local = "http://127.0.0.1:8000"; // local url
   static const folder_img_path =
-      "software buatan/fs/fs-laravel/public/storage/uploads/images"; // local url
+      "git/fs/fs-laravel/public/storage/uploads/images"; // local url
   static const folder_img_path_edit =
-      "software buatan/fs/fs-laravel/public/storage/uploads/images/edit"; // local url edit
+      "git/fs/fs-laravel/public/storage/uploads/images/edit"; // local url edit
 
   // colors wave
   static const _backgroundColor = "#43ff64d9";
